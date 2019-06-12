04f0f1503866d19621440e8fadf25a57c7103f2c
yukijina <yukijina@gmail.com>
2019-06-12 06:42:23 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+  return customerName = customerName.toUpperCase()
+}
+
+var bestCustomer;
+function setBestCustomer() {
+  return bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer() {
+  return bestCustomer = "maybe bob"
+}
+
+const leastFavoriteCustomer = "coco";
+function changeLeastFavoriteCustomer() {
+  return leastFavoriteCustomer = "Assignment to constatnt variable"
+}
